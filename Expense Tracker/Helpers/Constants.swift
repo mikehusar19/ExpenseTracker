61c6979d5f02dda38ef4fa5e56fb20770311e0e6
Michael Husar <michael.husar@tum.de>
2025-02-14 19:34:54 +0100

    Add IntroScreen sheet, add Tabview

---
+++ Expense Tracker/Helpers/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Expense Tracker
+//
+//  Created by Husar Michael on 14.02.25.
+//
+
+import SwiftUI
+
+// appTint which will be used all over the app
+let appTint: Color = .blue
